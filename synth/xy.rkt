10d2f73a9fb6e98bf60bf4985601663b246b4a86
Neea <neearusch@gmail.com>
2025-06-16 18:26:43 -0400

    interpreter

--- synth/xy.rkt
+++ synth/xy.rkt
@@ -1,7 +1,33 @@
 #lang rosette/safe
 
-; Require the sketching library.
-(require rosette/lib/synthax)
+(require rosette/lib/synthax)  ; sketching library
+(require rosette/lib/destruct) ; value destructuring
+
+; syntax
+(struct plus (left right) #:transparent)
+(struct mul  (left right) #:transparent)
+(struct square (arg)      #:transparent)
+
+; semantics
+(define (interpret p)
+  (destruct p
+    [(plus a b)  (+ (interpret a) (interpret b))]
+    [(mul a b)   (* (interpret a) (interpret b))]
+    [(square a)  (expt (interpret a) 2)]
+    [_ p]))
+
+;; symbolic variables
+(define-symbolic b c integer?)
+
+;; find one solution
+(solve (assert (= (interpret (mul c b)) (+ b b))))
+
+;; forall x
+(synthesize
+  #:forall (list b)
+  #:guarantee (assert (= (interpret (mul c b)) (+ b b))))
+
+;; ================================================
 
 ;; shorthand for 32-bit bitvector.
 (define int32? (bitvector 32))
@@ -9,9 +35,9 @@
 ;; int -> 32-bit bitvect.
 (define (INT i) (bv i int32?))
 
-;; Invariant grammar (of 2 variables)
+;; DSL (2-variable) invariant grammar
 (define-grammar (inv x y)  
-  ;; e := x | y | op e e | op c e
+  ;; e := x | y | op exp exp | op const exp
   [e (choose x y ((op) (e) (e)) ((op) (c) (e)))] 
   ;; op  :=  + | - | * | %
   [op (choose bvadd bvsub bvmul bvsmod)]
@@ -29,6 +55,7 @@
 (define (term v1 v2)
   (inv v1 v2 #:depth 2))
 
+;; variables
 (define-symbolic x y int32?)
 
 (define solution
@@ -36,6 +63,6 @@
     #:forall    (list x y)
     #:guarantee (same term x y)))
 
-
 (if (sat? solution) (print-forms solution) (print "UNSAT"))
+
 (print "DONE")
