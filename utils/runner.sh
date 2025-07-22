bf47da55f3419509348a2023352303d2c7839a5c
Neea <neearusch@gmail.com>
2025-07-22 00:19:31 -0400

    edit utils and inputs

--- utils/runner.sh
+++ utils/runner.sh
@@ -5,15 +5,14 @@ CMD="$2"
 LOG="$3"
 
 runCmdWithTimeout() {
-  (eval "$2") 2>>"$3" 1>/dev/null &
-  pid=$!
-  (sleep "$1" && kill -HUP $pid) 2>/dev/null &
-  watcher=$!
+  (eval "$2") 2>>"$3" 1>/dev/null & pid=$!
+  (sleep "$1" && kill -HUP $pid) 2>/dev/null & watcher=$!
   if wait $pid || ex=$? >/dev/null; then
     pkill -HUP -P $watcher
     wait $watcher
     echo $((ex))
   else echo 129; fi
+  exit 0;
 }
 
 res=$(runCmdWithTimeout "$TO" "$CMD" "$LOG");
--- utils/tracify.py
+++ utils/tracify.py
@@ -12,6 +12,8 @@ with open(sys.argv[1], 'r') as fp:
 
 entries = [x.strip().split(SEP1) for x in raw]
 cols = len(entries[0])
-
-print(entries)
-print(cols)
+n = max([max([len(x) for x in e]) for e in entries])
+d = [SEP2.join([x.rjust(n) for x in e]) for e in entries]
+h = [SEP2.join([('I '+chr(i)).rjust(n) for i in range(f0, f0 + cols)])]
+f = '\n'.join([t + x for x in h + d])
+print(f)
